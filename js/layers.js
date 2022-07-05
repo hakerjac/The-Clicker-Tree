2cdc7f76359dc101b7827bcb5b1970afe5e742ee
hackerjac <1587126777@qq.com>
2022-07-05 21:10:59 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -72,7 +72,7 @@ addLayer("c", {
         }
     }
 )
-if(hasUpgrade("c",23))addLayer("1", {
+addLayer("1", {
     name: "first", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "1", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -98,5 +98,5 @@ if(hasUpgrade("c",23))addLayer("1", {
     hotkeys: [
         {key: "1", description: "1: Reset for first pts", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true},
+    layerShown(){if(hasUpgrade("c",23)) return true},
 })
