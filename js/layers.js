48d8920a2cd08ca023fe2e45a76720899bfb7419
hackerjac <1587126777@qq.com>
2022-07-05 21:02:54 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -60,15 +60,43 @@ addLayer("c", {
             description: "******",
             cost: new Decimal(100),
             effect(){
-                return player[this.layer].points.add(1)
+                return player[this.layer].points.add(1).pow(0.5)
             },
             effectDisplay(){ return format(upgradeEffect(this.layer,this.id) + "x")}
         },
         23:{
             title: "结束新手教程",
             description: "新层级!!!",
-            cost: new Decimal(1e12500000)
+            cost: new Decimal(300)
         }
         }
     }
 )
+if(hasUpgrade("c",23))addLayer("1", {
+    name: "first", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "1", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: -100, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#FF0000",
+    requires: new Decimal(1), // Can be a function that takes requirement increases into account
+    resource: "first points", // Name of prestige currency
+    baseResource: "clickers", // Name of resource prestige is based on
+    baseAmount() {return player[clicker].points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() {
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "1", description: "1: Reset for first pts", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+})
